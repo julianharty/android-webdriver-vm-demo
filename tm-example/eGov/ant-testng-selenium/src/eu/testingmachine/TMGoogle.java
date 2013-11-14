12e2aecc45ade49f4d3e42b3122c095439269fbe
Julian Harty <julianharty@gmail.com>
2013-11-14 13:04:02 +0000

    Added dump of the exception stack trace to help diagnose the problems.

--- tm-example/eGov/ant-testng-selenium/src/eu/testingmachine/TMGoogle.java
+++ tm-example/eGov/ant-testng-selenium/src/eu/testingmachine/TMGoogle.java
@@ -21,22 +21,26 @@ public class TMGoogle {
 
   @Test
   public void testGoogle() throws Exception {
-    WebDriver driver = new AndroidDriver();
+    try {
+      WebDriver driver = new AndroidDriver();
     
-    // And now use this to visit Google
-    driver.get("http://www.google.com");
+      // And now use this to visit Google
+      driver.get("http://www.google.com");
     
-    // Find the text input element by its name
-    WebElement element = driver.findElement(By.name("q"));
+      // Find the text input element by its name
+      WebElement element = driver.findElement(By.name("q"));
     
-    // Enter something to search for
-    element.sendKeys("Cheese!");
+      // Enter something to search for
+      element.sendKeys("Cheese!");
     
-    // Now submit the form. WebDriver will find the form for us from the element
-    element.submit();
+      // Now submit the form. WebDriver will find the form for us from the element
+      element.submit();
     
-    // Check the title of the page
-    System.out.println("Page title is: " + driver.getTitle());
-    driver.quit();
+      // Check the title of the page
+      System.out.println("Page title is: " + driver.getTitle());
+      driver.quit();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
