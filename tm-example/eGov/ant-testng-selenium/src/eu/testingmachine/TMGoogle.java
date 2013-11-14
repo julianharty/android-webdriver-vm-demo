15ced68c590b5766ecb119c4de04631c17894e5c
Julian Harty <julianharty@gmail.com>
2013-11-14 12:50:45 +0000

    OK, so we can connect to the internet from travis. Now let's try a simple test to visit Google.
    The previous tests are not active at this stage, I expect them to fail until we diagnose the problem(s).

---
+++ tm-example/eGov/ant-testng-selenium/src/eu/testingmachine/TMGoogle.java
@@ -0,0 +1,42 @@
+package eu.testingmachine;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.android.AndroidDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.UnreachableBrowserException;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TMGoogle {
+
+  @Test
+  public void testGoogle() throws Exception {
+    WebDriver driver = new AndroidDriver();
+    
+    // And now use this to visit Google
+    driver.get("http://www.google.com");
+    
+    // Find the text input element by its name
+    WebElement element = driver.findElement(By.name("q"));
+    
+    // Enter something to search for
+    element.sendKeys("Cheese!");
+    
+    // Now submit the form. WebDriver will find the form for us from the element
+    element.submit();
+    
+    // Check the title of the page
+    System.out.println("Page title is: " + driver.getTitle());
+    driver.quit();
+  }
+}
